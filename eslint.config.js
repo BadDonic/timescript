07b1c3d05360ac246b9c5989cb10abe1ce9fc172
Danylo <danylod@wix.com>
2024-04-19 10:53:18 +0300

    added eslint packages

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+
+import path from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+import pluginJs from "@eslint/js";
+
+// mimic CommonJS variables -- not needed if using CommonJS
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+
+export default [
+  {languageOptions: { globals: globals.browser }, ignorePatterns: ["node_modules/", "**/*.test.ts","**/*.spec.ts"]},
+  ...compat.extends("standard-with-typescript"),
+
+];
